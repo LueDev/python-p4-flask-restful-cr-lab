65224061d964689e83bb54098e2b0fbb266b71e1
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-06-18 16:32:43 -0400

    All tests passing. Using the Flask API resource class is easy and readable

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -11,17 +12,69 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = True
 
+CORS(app)
+
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
 class Plants(Resource):
-    pass
+
+    def get(self):
+
+        response_dict_list = [n.to_dict() for n in Plant.query.all()]
+
+        response = make_response(
+            jsonify(response_dict_list),
+            200,
+        )
+
+        return response
+
+    def post(self):
+        data = request.json  # Extract data from JSON payload
+
+        new_record = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            201,
+        )
+
+        return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+
+    def get(self, id):
+
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            response_dict = plant.to_dict()
+            response = make_response(
+                jsonify(response_dict),
+                200,
+            )
+        else:
+            response = make_response(
+                {"error": "Plant not found"},
+                404,
+            )
+
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
